2e8ecc78fe2e08443b9df363871d4b7a6ea7f7af
BrandonC123 <brandoc318@gmail.com>
2022-05-20 12:28:19 -0700

    Added a check/x button for edit function. Currently console.log values next is to create actual tasks with constructor and store into array.

--- src/index.js
+++ src/index.js
@@ -19,20 +19,67 @@ const task = (title, description, dueDate, priority) => {
     };
 };
 
-const elementCreation = (() => {
-    const todayCard = document.getElementById("today-card")
-    function createTask() {
-        const task = document.createElement("div")
-        task.classList.add("task")
-        const taskTitle = document.createElement("div")
-        taskTitle.textContent = "dummy text"
-        const calendar = document.createElement("input")
-        calendar.type = "date"
-        task.appendChild(taskTitle)
-        task.appendChild(calendar)
-        todayCard.appendChild(task)
+const elementHandler = (() => {
+    let tasks = [];
+    function getTaskValues(id) {
+        tasks.push(id);
+        console.log(document.getElementById("calendar-" + id).value);
+        document.getElementById("task-actions-" + id).classList.add("hide")
+        // create task with construct, add to array
     }
-    document.querySelector(".action-btn").addEventListener("click", createTask)
+    return {
+        getTaskValues,
+        tasks,
+    };
 })();
 
-const displayElements = () => {};
+const elementCreation = (() => {
+    const todayCont = document.getElementById("today-container");
+
+    function createTask() {
+        const taskId = elementHandler.tasks.length + 1;
+        const task = document.createElement("div");
+        task.setAttribute("id", "task-" + taskId);
+        task.classList.add("task");
+
+        const taskTitle = document.createElement("input");
+        taskTitle.setAttribute("id", "taskTitle-" + taskId);
+        taskTitle.type = "text";
+        taskTitle.value = "dummy text";
+
+        const calendar = document.createElement("input");
+        calendar.setAttribute("id", "calendar-" + taskId);
+        calendar.type = "date";
+        calendar.value = "2022-05-03"
+
+        const taskActions = document.createElement("div");
+        taskActions.setAttribute("id", "task-actions-" + taskId);
+        taskActions.classList.add("task-actions");
+        const checkBtn = document.createElement("button");
+        checkBtn.setAttribute("id", "check-" + taskId);
+        checkBtn.classList.add("check-btn");
+        checkBtn.textContent = "✔";
+        const xBtn = document.createElement("button");
+        xBtn.setAttribute("id", "x-" + taskId);
+        xBtn.classList.add("x-btn");
+        xBtn.textContent = "X";
+        taskActions.appendChild(checkBtn);
+        taskActions.appendChild(xBtn);
+
+        task.appendChild(taskTitle);
+        task.appendChild(calendar);
+        task.appendChild(taskActions);
+        todayCont.appendChild(task);
+        // elementHandler.getTaskValues(taskId);
+        checkBtn.addEventListener("click", function () {
+            elementHandler.getTaskValues(taskId);
+        });
+    }
+    function toggleEdit(id) {
+
+    }
+    document.querySelector("#today-btn").addEventListener("click", createTask);
+    return {
+        createTask,
+    };
+})();
